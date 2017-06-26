9d688b9ca03a5f9cab0d4c6424de00825c85ed61
freepower311 <nikolai.nadezhin@gmail.com>
2017-06-26 18:21:29 +0300

    refactoring 3

--- teacher_stat.py
+++ teacher_stat.py
@@ -1,67 +1,64 @@
-def read_statics_from_file(path):
-    # make dictionary
-    teachers = {}
+# set file or path
+path = "./source.csv"
+
+def readStatFromFile(path):
+    # open file
     with open(path) as source:
         for line in source:
             line = line.strip().split(";")
-            sum_of_marks = 0
-            count_of_marks = 0
-            final_mark = 0
+
+            sumOfMarks = 0
+            countOfMarks = 0
+            finalMark = 0
             if line[0] == "Семестр":
-                source.readline()
-                new_line = source.readline().strip().split(";")
-                if new_line[0] == "Оцените качество преподавания курса по шкале от 1 до 5:":
-                    teacher = new_line[1]
-                    subject = new_line[2]
+                newLine = source.readline()
+                newLine = source.readline().strip().split(";")
+                if newLine[0] == "Оцените качество преподавания курса по шкале от 1 до 5:":
+                    teacher = newLine[1]
+                    subject = newLine[2]
                     marks = source.readline().strip().split(";")
                     for mark in marks:
-                        sum_of_marks += int(mark)
-                        count_of_marks += 1
-                    if count_of_marks > 0:
-                        final_mark = sum_of_marks / count_of_marks
+                        sumOfMarks += int(mark)
+                        countOfMarks += 1
+                    if countOfMarks > 0:
+                        finalMark = sumOfMarks / countOfMarks
                     if teacher in teachers:
                         if subject in teachers[teacher]:
-                            teachers[teacher][subject]["teacher"] = final_mark
+                            teachers[teacher][subject]["teacher"] = finalMark
                         else:
-                            teachers[teacher][subject] = {"teacher": final_mark}
+                            teachers[teacher][subject] = {"teacher": finalMark}
                     else:
-                        teachers[teacher] = {subject: {"teacher": final_mark}}
-                elif new_line[0] == "Оцените предмет по шкале от 1 до 5:":
-                    teacher = new_line[1]
-                    subject = new_line[2]
+                        teachers[teacher] = {subject: {"teacher": finalMark}}
+                elif newLine[0] == "Оцените предмет по шкале от 1 до 5:":
+                    teacher = newLine[1]
+                    subject = newLine[2]
                     marks = source.readline().strip().split(";")
                     for mark in marks:
-                        sum_of_marks += int(mark)
-                        count_of_marks += 1
-                    if count_of_marks > 0:
-                        final_mark = sum_of_marks / count_of_marks
+                        sumOfMarks += int(mark)
+                        countOfMarks += 1
+                    if countOfMarks > 0:
+                        finalMark = sumOfMarks / countOfMarks
                     if teacher in teachers:
                         if subject in teachers[teacher]:
-                            teachers[teacher][subject]["subject"] = final_mark
+                            teachers[teacher][subject]["subject"] = finalMark
                         else:
-                            teachers[teacher][subject] = {"subject": final_mark}
+                            teachers[teacher][subject] = {"subject": finalMark}
 
                     else:
-                        teachers[teacher] = {subject: {"subject": final_mark}}
+                        teachers[teacher] = {subject: {"subject": finalMark}}
                 else:
                     continue
     return teachers
+# make dict
+teachers = {}
 
 
-def write_statistics_to_file(teachers):
-    with open("result.csv", "w") as out:
-        out.write("Преподаватель;Предмет;Средняя оценка за предмет;Средняя оценка преподавателя\n")
-        for teacher in teachers:
-            for sub in teachers[teacher]:
-                out.write(teacher + ";" + sub + ";" + str(teachers[teacher][sub]["subject"])
-                          + ";" + str(teachers[teacher][sub]["teacher"]) + "\n")
+print(teachers)
+# write results
+with open("result.csv","w") as out:
+    out.write("Преподаватель;Предмет;Средняя оценка за предмет;Средняя оценка преподавателя\n")
+    for teacher in teachers:
+        for sub in teachers[teacher]:
+            out.write(teacher + ";" + sub + ";" + str(teachers[teacher][sub]["subject"])
+                                                      + ";" + str(teachers[teacher][sub]["teacher"] ) + "\n")
 
-
-def main():
-    local_path = "./source.csv"
-    teachers = read_statics_from_file(local_path)
-    write_statistics_to_file(teachers)
-
-
-if __name__ == "__main__":
-    main()
